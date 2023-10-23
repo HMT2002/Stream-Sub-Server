a044738569b6186f49f34b097eafbb7d29d6f383
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-10-23 08:57:11 +0700

    finish delete

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -28,20 +28,32 @@ exports.ReceiveDeleteRequest = catchAsync(async (req, res, next) => {
 });
 
 exports.ReceiveDeleteFolderRequest = catchAsync(async (req, res, next) => {
-  const filename = req.body.filename || 'largetest.mp4';
-  const videoPath = 'videos/' + filename;
+  const filename = req.body.filename || '7ZXNZYOHls';
+  const videoFolderPath = 'videos/' + filename;
   const url = req.body.url || 'http://localhost';
   const port = req.body.port || ':9200';
-  if (!fs.existsSync(videoPath)) {
-    console.log('not found video');
+  if (!fs.existsSync(videoFolderPath)) {
+    console.log('not found path');
     res.status(201).json({
-      message: 'video not found on ' + url + port + ' path: ' + videoPath,
+      message: 'path not found on ' + url + port + ' path: ' + videoFolderPath,
     });
   } else {
-    console.log('found video, deleting...');
-    fs.unlinkSync(videoPath);
+    console.log('found path, deleting...');
+    
+    const fileList = fs.readdirSync(videoFolderPath);
+    console.log(fileList);
+    for (let i = 0; i < fileList.length; i++) {
+      const videoPath = videoFolderPath + '/' + fileList[i];
+      console.log(videoPath);
+      fs.unlinkSync(videoPath);
+
+    }
+    fs.rmdir(videoFolderPath,() => { 
+      console.log("Folder Deleted!"); 
+    }); 
+
     res.status(201).json({
-      message: 'video deleted on ' + url + port + ' path: ' + videoPath,
+      message: 'path deleted on ' + url + port + ' path: ' + videoFolderPath,
     });
   }
 });
