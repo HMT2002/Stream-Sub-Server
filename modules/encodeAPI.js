c0eadd8ea97531ad98f0b06f6fd7b4e066e30a26
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2023-12-24 15:42:40 +0700

    hot fix

--- modules/encodeAPI.js
+++ modules/encodeAPI.js
@@ -175,8 +175,12 @@ const encodeIntoDashVer2 = async (destination, originalname, statusID) => {
     ' -map 0:v -map 0:a -preset ultrafast -crf 28 -c:s srt -sn -c:v libx264 -c:a copy -vf format=yuv420p -b:v:0 300k -s:v:0 720x480 -b:v:1 700k -s:v:1 1080x720 -b:v:2 1300k -s:v:2 1920x1080 -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -single_file 0 -use_template 1 -seg_duration 10 -init_seg_name init_$RepresentationID$.m4s -media_seg_name chunk_$RepresentationID$_$Number%05d$.m4s -f dash ' +
     outputResult;
   fluentFfmpeg.ffprobe(filePath, function (error, metadata) {
+    if (error) {
+      console.log(error);
+      return;
+    }
     console.log('Video duration: ' + metadata.format.duration);
-    videoDuration = metadata.format.duration;
+    videoDuration = metadata.format.duration || 0;
   });
   executeFfmpeg(command1)
     .on('start', async (commandLine) => {
