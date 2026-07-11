62d3b7f43b301babeecedf75d40358d086710d5c
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2026-07-11 16:26:19 +0700

    Update defaultRoute.js

--- routes/defaultRoute.js
+++ routes/defaultRoute.js
@@ -8,7 +8,6 @@ router.route('/check/alive/is-this-alive').get(defaultController.CheckIfThisServ
 router.route('/check/hls/:filename').get(defaultController.CheckHlsFile);
 router.route('/check/dash/:filename').get(defaultController.CheckDashFile);
 
-
 // router.route('/check/hls/:filename').post(defaultController.CheckHlsFile);
 // router.route('/check/dash/:filename').post(defaultController.CheckDashFile);
 
